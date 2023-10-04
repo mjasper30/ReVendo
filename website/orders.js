fbf22c3a91c430c628dc86dcb0554cd2d99a7b87
mjasper30 <jasper.macaraeg42@gmail.com>
2023-10-04 12:33:35 +0800

    dark mode and dashboard is done

---
+++ website/orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "546471",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "LARVENDER KF102 Drone",
+    productNumber: "346415",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "549715",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "546471",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "DJI Air 25",
+    productNumber: "897415",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lozenge Drone",
+    productNumber: "789451",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
