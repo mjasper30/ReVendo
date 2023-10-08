8f2f35af811daa174cec8353519f86ddca047364
mjasper30 <jasper.macaraeg42@gmail.com>
2023-10-08 22:53:21 +0800

    practice

---
+++ practice/src/ManyComponents.jsx
@@ -0,0 +1,7 @@
+export function Button() {
+  return <button>Button</button>;
+}
+
+export function Header() {
+  return <h1>Header</h1>;
+}
--- practice/src/main.jsx
+++ practice/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import MainComponent from "./MainComponent.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <MainComponent />
   </React.StrictMode>
 );
