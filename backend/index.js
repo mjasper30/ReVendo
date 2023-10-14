cc605e6d94754dc58fbbdac57b7cc93c80788e9c
mjasper30 <jasper.macaraeg42@gmail.com>
2023-10-14 23:20:01 +0800

    goods

--- backend/index.js
+++ backend/index.js
@@ -36,10 +36,9 @@ app.get("/books", (req, res) => {
 app.post("/books", (req, res) => {
   const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
   const values = [req.body.title, req.body.desc, req.body.cover];
-  // const values = ["dsadas", "dsadas", "dsadas"];
 
   db.query(q, [values], (err, data) => {
-    if (err) return res.json("DSADSADA");
+    if (err) return res.json(err);
     return res.json("New book added!");
   });
 });
