928d3d991f5a4827a50ae4725dc431a41bb400d8
mjasper30 <jasper.macaraeg42@gmail.com>
2023-10-14 22:50:07 +0800

    to be continue

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+// Create connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "revendo",
+  database: "test",
+});
+
+app.use(express.json());
+
+// Connect
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("MySQL Connected...");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+  // const values = ["dsadas", "dsadas", "dsadas"];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json("DSADSADA");
+    return res.json("New book added!");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
