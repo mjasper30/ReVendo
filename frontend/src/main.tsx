512458c36add922b20c0ee9a036b70828e62f276
mjasper30 <jasper.macaraeg42@gmail.com>
2023-10-16 21:21:04 +0800

    browser router applied

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import App from "./App.tsx";
-import Login from "./Login.tsx";
-import SignUp from "./SignUp.tsx";
-import Dashboard from "./Dashboard.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>
 );
