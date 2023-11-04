5b8a24c0bbe9bd95ada2d14e60ef24baad3631e5
mjasper30 <jasper.macaraeg42@gmail.com>
2023-11-05 00:54:34 +0800

    dashboard done just break the components later

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,5 +1,5 @@
 import logo from "./assets/Revendo_logo.png";
-import { Avatar, Dropdown, Navbar, Tooltip } from "flowbite-react";
+import { Avatar, Card, Dropdown, Navbar, Tooltip, Table } from "flowbite-react";
 import "./css/index.css";
 import { Link } from "react-router-dom";
 
@@ -108,6 +108,159 @@ export default function Dashboard() {
         <div className="flex flex-col ml-24 mx-auto">
           <div className="font-bold text-xl text-white ml-10 my-10 z-100">
             Dashboard
+          </div>
+          <div className="flex items-center justify-between ml-10 mb-5 gap-4">
+            <Card className="max-w-sm bg-green-500" horizontal>
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <span className="material-symbols-rounded text-[#2B2D31] p-2">
+                  manage_accounts
+                </span>
+                Registered RFID
+              </h5>
+              <p className="text-center font-normal text-xl text-gray-700 dark:text-gray-400">
+                456
+              </p>
+            </Card>
+            <Card className="max-w-sm bg-red-500" horizontal>
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <span className="material-symbols-rounded text-[#2B2D31] p-2">
+                  water_bottle
+                </span>
+                Plastic Bottles
+              </h5>
+              <p className="text-center font-normal text-xl text-gray-900 dark:text-gray-400">
+                7489412
+              </p>
+            </Card>
+            <Card className="max-w-sm bg-orange-500" horizontal>
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <span className="material-symbols-rounded text-[#2B2D31] p-2">
+                  inventory_2
+                </span>
+                Capacity Status
+              </h5>
+              <p className="text-center font-normal text-xl text-gray-700 dark:text-gray-400">
+                Not full
+              </p>
+            </Card>
+            <Card className="max-w-sm bg-violet-500" horizontal>
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <span className="material-symbols-rounded text-[#2B2D31] p-2">
+                  battery_charging_full
+                </span>
+                Battery Status
+              </h5>
+              <p className="text-center font-normal text-xl text-gray-700 dark:text-gray-400">
+                56%
+              </p>
+            </Card>
+          </div>
+          <div className="flex ml-10 mb-5 gap-4">
+            <Table className="text-center" striped hoverable>
+              <Table.Head className="bg-slate-600">
+                <Table.HeadCell>#</Table.HeadCell>
+                <Table.HeadCell>RFID Number</Table.HeadCell>
+                <Table.HeadCell>Points</Table.HeadCell>
+                <Table.HeadCell>Status</Table.HeadCell>
+                <Table.HeadCell>Action</Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    1
+                  </Table.Cell>
+                  <Table.Cell>33a537c2</Table.Cell>
+                  <Table.Cell>45</Table.Cell>
+                  <Table.Cell>Active</Table.Cell>
+                  <Table.Cell>
+                    <i>
+                      <span className="material-symbols-rounded text-green-600 p-2">
+                        edit
+                      </span>
+                    </i>
+                    <i>
+                      <span className="material-symbols-rounded text-red-600 p-2">
+                        delete
+                      </span>
+                    </i>
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    2
+                  </Table.Cell>
+                  <Table.Cell>737832c2</Table.Cell>
+                  <Table.Cell>23</Table.Cell>
+                  <Table.Cell>Active</Table.Cell>
+                  <Table.Cell>
+                    <i>
+                      <span className="material-symbols-rounded text-green-600 p-2">
+                        edit
+                      </span>
+                    </i>
+                    <i>
+                      <span className="material-symbols-rounded text-red-600 p-2">
+                        delete
+                      </span>
+                    </i>
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    3
+                  </Table.Cell>
+                  <Table.Cell>e3153ec2</Table.Cell>
+                  <Table.Cell>15</Table.Cell>
+                  <Table.Cell>Active</Table.Cell>
+                  <Table.Cell>
+                    <i>
+                      <span className="material-symbols-rounded text-green-600 p-2">
+                        edit
+                      </span>
+                    </i>
+                    <i>
+                      <span className="material-symbols-rounded text-red-600 p-2">
+                        delete
+                      </span>
+                    </i>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+            <Table striped hoverable className="text-center">
+              <Table.Head className="bg-slate-600">
+                <Table.HeadCell>Unit Number</Table.HeadCell>
+                <Table.HeadCell>Capacity</Table.HeadCell>
+                <Table.HeadCell>Location</Table.HeadCell>
+                <Table.HeadCell>Battery</Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    1
+                  </Table.Cell>
+                  <Table.Cell>Full</Table.Cell>
+                  <Table.Cell>Caloocan</Table.Cell>
+                  <Table.Cell>100%</Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    2
+                  </Table.Cell>
+                  <Table.Cell>Not Full</Table.Cell>
+                  <Table.Cell>Malabon</Table.Cell>
+                  <Table.Cell>50%</Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    3
+                  </Table.Cell>
+                  <Table.Cell>Full</Table.Cell>
+                  <Table.Cell>Navotas</Table.Cell>
+                  <Table.Cell>60%</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
           </div>
         </div>
       </div>
