41da9a13bb27fa7da81c33f868db1f50655e15ab
mjasper30 <jasper.macaraeg42@gmail.com>
2023-11-06 20:05:57 +0800

    bug fixed

--- desktop.py
+++ desktop.py
@@ -11,14 +11,44 @@ label = None
 get_points_button = None
 check_balance_button = None
 image_button = None
+image_label = None
+new_button = None
+tutorial_header = None
+tutorial_steps = None
+button = None
 
 
 def trigger_script():
     os.system('python hello.py')
 
 
+def destroy_elements():
+    revendo_logo.place_forget()
+    label.place_forget()
+    get_points_button.place_forget()
+    check_balance_button.place_forget()
+    image_button.place_forget()
+    new_button.place_forget()
+    image_label.place_forget()
+    tutorial_header.place_forget()
+
+    menu()
+
+
+def destroy_elements1():
+    revendo_logo.place_forget()
+    label.place_forget()
+    get_points_button.place_forget()
+    check_balance_button.place_forget()
+    button.place_forget()
+    tutorial_header.place_forget()
+    tutorial_steps.place_forget()
+
+    menu()
+
+
 def scan_rfid():
-    global revendo_logo, label, get_points_button, check_balance_button, image_button
+    global revendo_logo, label, get_points_button, check_balance_button, image_button, image_label, new_button, tutorial_header, tutorial_steps, button
 
     tutorial_header = tk.Label(
         app, text="Scan your ReVendo Card", font=("Arial", 24), bg='white')
@@ -33,7 +63,7 @@ def scan_rfid():
     image_label.place(relx=0.5, rely=0.5, anchor='center')
 
     new_button = tk.Button(app, text="Cancel", font=(
-        "Arial", 16), command=menu, bg='SystemButtonFace', padx=20)
+        "Arial", 16), command=destroy_elements, bg='SystemButtonFace', padx=20)
     new_button.place(relx=0.5, rely=0.8, anchor='center',
                      width=150, height=40)
 
@@ -46,13 +76,14 @@ def scan_rfid():
 
 
 def check_points_page():
-    global revendo_logo, label, get_points_button, check_balance_button, image_button
+    global revendo_logo, label, get_points_button, check_balance_button, image_button, image_label, new_button, tutorial_header, tutorial_steps, button
+
     tutorial_header = tk.Label(
         app, text="Scan your ReVendo Card", font=("Arial", 24), bg='white')
     tutorial_header.place(relx=0.5, rely=0.2, anchor='center')
 
     new_button = tk.Button(app, text="Cancel", font=(
-        "Arial", 16), command=menu, bg='SystemButtonFace', padx=20)
+        "Arial", 16), command=destroy_elements, bg='SystemButtonFace', padx=20)
     new_button.place(relx=0.5, rely=0.8, anchor='center',
                      width=150, height=40)
 
@@ -73,19 +104,21 @@ def check_points_page():
 
 
 def tutorial_page():
+    global revendo_logo, label, get_points_button, check_balance_button, image_button, image_label, new_button, tutorial_header, tutorial_steps, button
+
     tutorial_header = tk.Label(
-        app, text="How to use ReVendo Machine", font=("Arial", 24), bg='white')
+        app, text="How to use ReVendo Machine", font=("Arial", 20), bg='white')
     tutorial_header.place(relx=0.5, rely=0.2, anchor='center')
 
     tutorial_steps = tk.Label(
-        app, text="Step 1: \nTap your card on the reader \n\n Step 2: \nInsert your plastic bottles inside the machine \n\n Step 3: \nCheck your points \n\n Step 4: \nOnce done, get your card and proceed to \ncharging station", font=("Arial", 18), bg='white')
+        app, text="Step 1: \nTap your card on the reader \n\n Step 2: \nInsert your plastic bottles inside the machine \n\n Step 3: \nCheck your points \n\n Step 4: \nOnce done, get your card and proceed to charging station", font=("Arial", 12), bg='white')
     tutorial_steps.place(relx=0.5, rely=0.5, anchor='center')
 
     button = tk.Button(app, text="Okay", font=("Arial", 16),
-                       command=menu, bg='SystemButtonFace', padx=20)
+                       command=destroy_elements1, bg='SystemButtonFace', padx=20)
     button.place(relx=0.5, rely=0.8, anchor='center', width=150, height=40)
 
-    # Hiding specific elements
+   # Hiding specific elements
     revendo_logo.place_forget()
     label.place_forget()
     get_points_button.place_forget()
@@ -94,7 +127,7 @@ def tutorial_page():
 
 
 def menu():
-    global revendo_logo, label, get_points_button, check_balance_button, image_button
+    global revendo_logo, label, get_points_button, check_balance_button, image_button, image_label, new_button, tutorial_header, tutorial_steps, button
 
     revendo_logo_path = "Revendo_logo.png"
     center_revendo_logo = Image.open(revendo_logo_path)
@@ -113,12 +146,12 @@ def menu():
     get_points_button = tk.Button(app, text="Get Points", font=("Arial", 16),
                                   command=scan_rfid, bg='SystemButtonFace', padx=20)
     get_points_button.place(
-        relx=0.3, rely=0.6, anchor='center', width=130, height=40)
+        relx=0.3, rely=0.7, anchor='center', width=130, height=40)
 
     check_balance_button = tk.Button(app, text="Check Balance", font=("Arial", 16),
                                      command=check_points_page, bg='SystemButtonFace', padx=20)
     check_balance_button.place(
-        relx=0.7, rely=0.6, anchor='center', width=160, height=40)
+        relx=0.7, rely=0.7, anchor='center', width=160, height=40)
 
     tutorial_button_path = "question.png"
     tutorial_button = Image.open(tutorial_button_path)
@@ -129,7 +162,7 @@ def menu():
                              command=tutorial_page, border=0)
 
     image_button.image = photo_button
-    image_button.place(relx=0.5, rely=0.7, anchor='center')
+    image_button.place(relx=0.5, rely=0.75, anchor='center')
 
 
 # Get the screen width and height
