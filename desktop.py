675386cf2dfa042d2d87e22153366ecf3824c728
mjasper30 <jasper.macaraeg42@gmail.com>
2023-11-07 12:52:05 +0800

    process plastic bottle menu

--- desktop.py
+++ desktop.py
@@ -75,6 +75,111 @@ def scan_rfid():
     image_button.place_forget()
 
 
+def process_plastic_bottles():
+    tutorial_header = tk.Label(
+        app, text="Balance", font=("Arial", 24), bg='white')
+    tutorial_header.place(relx=0.2, rely=0.1, anchor='center')
+
+    image_path = "process_plastic_bottle.png"
+    img = Image.open(image_path)
+    img = img.resize((380, 350))
+    photo = ImageTk.PhotoImage(img)
+    image_label = tk.Label(app, image=photo)
+    image_label.image = photo
+    image_label.place(relx=0.3, rely=0.55, anchor='center')
+
+    balance_text = tk.Entry(app, state='disabled', font=(
+        "Arial", 16), bg='SystemButtonFace', justify='center')
+    balance_text.insert(0, "Your balance")
+    balance_text.place(relx=0.6, rely=0.1, anchor='center',
+                       width=400, height=40)
+
+    # Simulating adding a value to the balance
+    # Assume the balance value is retrieved from a function or variable
+    balance_value = 465464  # Example value
+    balance_text.config(state='normal')
+    balance_text.delete(0, 'end')  # Clear the current value
+    balance_text.insert(0, str(balance_value))  # Insert the new value
+    balance_text.config(state='disabled')
+
+    smallbottle_text = tk.Entry(app, state='disabled', font=(
+        "Arial", 16), bg='SystemButtonFace', justify='center')
+    smallbottle_text.insert(0, "Your balance")
+    smallbottle_text.place(relx=0.21, rely=0.35, anchor='center',
+                           width=60, height=40)
+
+    smallbot_value = 3  # Example value
+    smallbottle_text.config(state='normal')
+    smallbottle_text.delete(0, 'end')  # Clear the current value
+    smallbottle_text.insert(0, str(smallbot_value))  # Insert the new value
+    smallbottle_text.config(state='disabled')
+
+    totalplasticbottle_text = tk.Entry(app, state='disabled', font=(
+        "Arial", 16), bg='SystemButtonFace', justify='center')
+    totalplasticbottle_text.insert(0, "Your balance")
+    totalplasticbottle_text.place(relx=0.41, rely=0.35, anchor='center',
+                                  width=100, height=40)
+
+    totalplasticbottle_value = 18  # Example value
+    totalplasticbottle_text.config(state='normal')
+    totalplasticbottle_text.delete(0, 'end')  # Clear the current value
+    totalplasticbottle_text.insert(
+        0, str(totalplasticbottle_value))  # Insert the new value
+    totalplasticbottle_text.config(state='disabled')
+
+    mediumbottle_text = tk.Entry(app, state='disabled', font=(
+        "Arial", 16), bg='SystemButtonFace', justify='center')
+    mediumbottle_text.insert(0, "Your balance")
+    mediumbottle_text.place(relx=0.21, rely=0.6, anchor='center',
+                            width=60, height=40)
+
+    mediumbottle_value = 3  # Example value
+    mediumbottle_text.config(state='normal')
+    mediumbottle_text.delete(0, 'end')  # Clear the current value
+    mediumbottle_text.insert(0, str(mediumbottle_value)
+                             )  # Insert the new value
+    mediumbottle_text.config(state='disabled')
+
+    totalpoints_text = tk.Entry(app, state='disabled', font=(
+        "Arial", 16), bg='SystemButtonFace', justify='center')
+    totalpoints_text.insert(0, "Your balance")
+    totalpoints_text.place(relx=0.41, rely=0.6, anchor='center',
+                           width=100, height=40)
+
+    totalpoints_value = 123  # Example value
+    totalpoints_text.config(state='normal')
+    totalpoints_text.delete(0, 'end')  # Clear the current value
+    totalpoints_text.insert(
+        0, str(totalpoints_value))  # Insert the new value
+    totalpoints_text.config(state='disabled')
+
+    smallbottle_text = tk.Entry(app, state='disabled', font=(
+        "Arial", 16), bg='SystemButtonFace', justify='center')
+    smallbottle_text.insert(0, "Your balance")
+    smallbottle_text.place(relx=0.21, rely=0.82, anchor='center',
+                           width=60, height=40)
+
+    smallbottle_value = 12  # Example value
+    smallbottle_text.config(state='normal')
+    smallbottle_text.delete(0, 'end')  # Clear the current value
+    smallbottle_text.insert(0, str(smallbottle_value)
+                            )  # Insert the new value
+    smallbottle_text.config(state='disabled')
+
+    new_button = tk.Button(app, text="Cancel", font=(
+        "Arial", 16), command=destroy_elements, bg='SystemButtonFace', padx=20)
+    new_button.place(relx=0.5, rely=0.8, anchor='center',
+                     width=150, height=40)
+
+    # Hiding specific elements
+    revendo_logo.place_forget()
+    label.place_forget()
+    get_points_button.place_forget()
+    check_balance_button.place_forget()
+    image_button.place_forget()
+    new_button.place_forget()
+
+
 def check_points_page():
     global revendo_logo, label, get_points_button, check_balance_button, image_button, image_label, new_button, tutorial_header, tutorial_steps, button
 
@@ -159,7 +264,7 @@ def menu():
     photo_button = ImageTk.PhotoImage(tutorial_button)
 
     image_button = tk.Button(app, image=photo_button,
-                             command=tutorial_page, border=0)
+                             command=process_plastic_bottles, border=0)
 
     image_button.image = photo_button
     image_button.place(relx=0.5, rely=0.75, anchor='center')
