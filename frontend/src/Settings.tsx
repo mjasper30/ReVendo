ec260c990ce7fe1ecf4dd72690602d4e248cc403
John Kenneth Adriano <jkadriano2002@gmail.com>
2023-11-15 15:38:58 +0800

    mostly settings stuff. added a slider (to be fixed) and some toggle buttons

--- frontend/src/Settings.tsx
+++ frontend/src/Settings.tsx
@@ -1,9 +1,18 @@
 import SidebarComponent from "./component/SidebarComponent";
 import NavbarComponent from "./component/NavbarComponent";
+import { ToggleSwitch } from 'flowbite-react';
+import { Label, RangeSlider} from 'flowbite-react';
+import { useState } from 'react';
 import "./css/index.css";
 
 export default function Settings() {
+
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+  const [switch4, setSwitch4] = useState(false);
   return (
+
     <div className="h-full w-screen flex">
       <SidebarComponent />
 
@@ -14,10 +23,36 @@ export default function Settings() {
           <div className="w-full sm:w-4/5">
             <div className="font-bold text-xl text-white my-10 z-100">
               Settings
+              </div>
+              <div className="font-bold text-m text-white my-10 z-100">
+                Administrative Settings
+              </div>
+              <div className="flex max-w-md flex-col gap-4 p-4">
+                <ToggleSwitch checked={switch1} label="Disable Website for Maintenance" onChange={setSwitch1} />
+                </div>
+              <div className="font-bold text-m text-white my-10 z-100">
+                Appearance
+              </div>
+               <div className="flex max-w-md flex-col gap-4 p-4">
+                <ToggleSwitch checked={switch2} label="Dark Mode" onChange={setSwitch2} />
+                </div>
+                <div className="flex max-w-md flex-col gap-4 p-4">
+                <ToggleSwitch checked={switch3} label="Among Gus" onChange={setSwitch3} />
+                </div>
+                <div className="flex max-w-md flex-col gap-4 p-4">
+                <ToggleSwitch checked={switch4} label="Fortnite" onChange={setSwitch4} />
+                </div>
+                <div className="flex max-w-md flex-col gap-4">
+                <div>
+                  <div className="mb-1 block">
+                    <Label htmlFor="default-range" value="Font Size" />
+                  <RangeSlider id="default-range" />
+                </div>
+                </div>
+                </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
