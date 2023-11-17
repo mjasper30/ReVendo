bbb65e9e734429aeca9b95d7df6c1814260de966
mjasper30 <jasper.macaraeg42@gmail.com>
2023-11-17 21:17:30 +0800

    loader responsive

--- frontend/src/Loader.tsx
+++ frontend/src/Loader.tsx
@@ -1,4 +1,5 @@
 import "./css/loader.css";
+
 function Loader() {
   return (
     <>
@@ -6,13 +7,17 @@ function Loader() {
         <div className="bg-background fixed inset-0 bg-cover bg-center fade-in">
           <div className="flex justify-center items-center flex-col pt-11">
             <img
-              className="firstLoop bounce2"
+              className="firstLoop bounce2 max-w-full h-auto"
               src="/src/assets/Revendo.png"
-              style={{ width: "480px", height: "480px" }}
+              alt="ReVendo Logo"
             />
-            <div className="content mt-14 tablet:text-[32px] laptop:text-[37px]">
-              <h2>ReVendo</h2>
-              <h2>ReVendo</h2>
+            <div className="content mt-14 text-center">
+              <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+                ReVendo
+              </h2>
+              <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+                ReVendo
+              </h2>
             </div>
           </div>
         </div>
