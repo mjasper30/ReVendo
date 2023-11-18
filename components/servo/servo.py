810961085c165fd33b2c61c61db23c6c752e4a8b
mjasper30 <jasper.macaraeg42@gmail.com>
2023-11-18 17:12:06 +0800

    servo by daniel sigue

---
+++ components/servo/servo.py
@@ -0,0 +1,17 @@
+from gpiozero import AngularServo, Device
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+
+# Set up pigpio pin factory
+Device.pin_factory = PiGPIOFactory(host='localhost', port=8888)
+
+# Your existing code
+servo = AngularServo(26, min_pulse_width=0.0006, max_pulse_width=0.0023)
+
+while True:
+    servo.angle = 90
+    sleep(2)
+    servo.angle = 0
+    sleep(2)
+    servo.angle = -90
+    sleep(2)
