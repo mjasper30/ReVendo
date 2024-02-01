3de12d3e5e8be3cf40304e5fd66e35f2e7e341ce
mjasper30 <jasper.macaraeg42@gmail.com>
2024-02-01 11:52:16 +0800

    break yolo process into functions

--- desktop/desktop_pi.py
+++ desktop/desktop_pi.py
@@ -8,9 +8,16 @@ from mfrc522 import SimpleMFRC522
 import requests
 import time
 import RPi.GPIO as GPIO
+import math
+from ultralytics import YOLO
+from gpiozero import AngularServo, Device
+from gpiozero.pins.pigpio import PiGPIOFactory
+from hx711 import HX711
 
 # API endpoints
 url_check_rfid = "http://192.168.68.111:3001/check_rfid"
+url_update_data = "http://192.168.68.111:3001/addDataHistory"
+url_update_points = "http://192.168.68.111:3001/updatePoints"
 
 global_points = 0
 scan_processed = True  # Flag to indicate if a scan has been processed
@@ -20,6 +27,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 buzzer_pin = 18
 GPIO.setup(buzzer_pin, GPIO.OUT)
+
+# Camera and object detection setup
+fov_vertical_degrees = 60
+distance = 19
+plastic_bottles_detected = 0
+total_small, total_medium, total_large = 0, 0, 0
 
 app = tk.Tk()
 app.title("ReVendo")
@@ -42,16 +55,188 @@ okay_button = None
 balance_label = None
 points_entry = None
 
+# HX711 setup and calibration
+hx = HX711(dout_pin=6, pd_sck_pin=5)
+calibration_file = "calibration_data.txt"
+
+
+def calibration_test():
+    # Reading if there is existing calibration file
+    try:
+        with open(calibration_file, "r") as file:
+            ratio = float(file.read().strip())
+            hx.set_scale_ratio(ratio)
+    except FileNotFoundError:
+        print("Calibration file not found. Performing calibration.")
+        hx.zero()
+        input("Place a known weight on the scale and press Enter: ")
+        reading = hx.get_data_mean(readings=100)
+        value = float(
+            input("Enter the known weight in grams and press Enter: "))
+        ratio = reading / value
+        hx.set_scale_ratio(ratio)
+        with open(calibration_file, "w") as file:
+            file.write(str(ratio))
+
+
+def detect_weight():
+    # Weight check
+    # weight = hx.get_weight_mean()
+    # print(abs(int(weight)) - 344)
+
+    # For weight value testing purposes
+    weight = 17  # Replace with actual weight reading
+    print(weight)
+
+    # Check if the value of weight if greater than expected weight value
+    if abs(weight) >= 9:
+        GPIO.output(buzzer_pin, True)
+        time.sleep(0.2)
+        GPIO.output(buzzer_pin, False)
+        time.sleep(0.5)
+    else:
+        pass
+
+
+def yolo_process():
+    # Capturing of image script execution
+    subprocess.run(['bash', 'capture_image.sh'], check=True)
+
+    # Image processing
+    IMAGE_DIR = os.path.join('.', 'capture')
+    output_dir = os.path.join('.', 'output')
+    os.makedirs(output_dir, exist_ok=True)
+    model = YOLO(os.path.join('.', 'model', 'best4.pt'))
+    threshold, pixels_to_cm = 0.5, 0.0264583333
+
+    for image_file in [f for f in os.listdir(IMAGE_DIR) if f.endswith(('.jpg', '.png', '.jpeg'))]:
+        image_path = os.path.join(IMAGE_DIR, image_file)
+        frame = cv2.imread(image_path)
+        image_height = frame.shape[0]
+        fov_vertical_radians = math.radians(fov_vertical_degrees)
+        height_cm = int(
+            (image_height / frame.shape[0]) * distance * math.tan(fov_vertical_radians / 2) * 2)
+        results = model(frame)[0]
+
+        for result in results.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = result
+            if score > threshold:
+                plastic_bottles_detected += 1
+                height_cm = int((y2 - y1) * pixels_to_cm)
+                cv2.rectangle(frame, (int(x1), int(y1)),
+                              (int(x2), int(y2)), (0, 255, 0), 4)
+                text = f'Height: {height_cm} '
+                cv2.putText(frame, text, (int(x2) + 10, int(y1) + 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1, cv2.LINE_AA)
+                class_label = results.names[int(class_id)].upper()
+                cv2.putText(frame, class_label, (int(x1), int(
+                    y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1, cv2.LINE_AA)
+
+        output_image_path = os.path.join(output_dir, image_file)
+        cv2.imwrite(output_image_path, frame)
+        print("Object detection on images completed.")
+        print(height_cm)
+
+
+def object_classification():
+    # Object classification
+    size_of_object = ""
+    if weight <= 0 or height_cm == 21:
+        size_of_object = "No Object"
+        height_cm = 0
+        GPIO.output(buzzer_pin, True)
+        time.sleep(0.5)
+        GPIO.output(buzzer_pin, False)
+    elif weight >= 57:
+        size_of_object = "Heavy Object"
+        height_cm = 0
+    elif weight <= 8:
+        size_of_object = "Light Object"
+        height_cm = 0
+        GPIO.output(buzzer_pin, True)
+        time.sleep(0.5)
+        GPIO.output(buzzer_pin, False)
+    elif height_cm >= 8:
+        total_large += 1
+        size_of_object = "Large"
+        GPIO.output(buzzer_pin, True)
+        time.sleep(0.2)
+        GPIO.output(buzzer_pin, False)
+        servo.angle = 90
+        time.sleep(2)
+    elif height_cm >= 5:
+        total_medium += 1
+        size_of_object = "Medium"
+        GPIO.output(buzzer_pin, True)
+        time.sleep(0.2)
+        GPIO.output(buzzer_pin, False)
+        servo.angle = 90
+        time.sleep(2)
+    elif height_cm >= 2:
+        total_small += 1
+        size_of_object = "Small"
+        GPIO.output(buzzer_pin, True)
+        time.sleep(0.2)
+        GPIO.output(buzzer_pin, False)
+        servo.angle = 90
+        time.sleep(2)
+
+
+def reset_servo():
+    # Reset servo angle
+    servo.angle = 0
+    time.sleep(2)
+
+
+def insert_data():
+    # API send data
+    data = {'rfid': rfid_uid, 'height': height_cm, 'weight': int(
+        weight), 'size': size_of_object, 'no_object': 'no' if not results.boxes.data.tolist() else 'yes'}
+    files = {'image': open(image_path, 'rb')}
+    response_update_data = requests.post(
+        url_update_data, data=data, files=files)
+    if response_update_data.status_code == 200:
+        print("Data sent to the server successfully.")
+    else:
+        print("Failed to send data to the server.")
+
+
+def do_you_want_to_continue():
+    # User prompt to continue
+    choice = input("Do you want to continue (y/n)? ")
+    if choice.lower() != "y":
+        print("Total Small Plastic Bottle:", total_small)
+        print("Total Medium Plastic Bottle:", total_medium)
+        print("Total Large Plastic Bottle:", total_large)
+        print("Total Plastic Bottles Detected:", plastic_bottles_detected)
+        total_points = (total_large * 3) + (total_medium * 2) + total_small
+        print("Total Points:", total_points)
+
+        # API update points
+        data_to_update_points = {'rfid': rfid_uid,
+                                 'additionalPoints': total_points}
+        response_update_points = requests.post(
+            url_update_points, json=data_to_update_points)
+
+        if response_update_points.status_code == 200:
+            print("Points updated in the database successfully.")
+        else:
+            print("Failed to update points in the database.")
+
 # Function to update points on the UI
+
+
 def update_points():
     points_var.set(str(global_points))
     global scan_processed
     scan_processed = True  # Set the flag to indicate that the scan has been processed
 
 # Handle RFID scan in check balance
+
+
 def handle_rfid_scan():
     global global_points, scan_processed
-    
+
     try:
         # RFID setup
         reader = SimpleMFRC522()
@@ -63,7 +248,8 @@ def handle_rfid_scan():
         # Process scan only if the flag is True
         if scan_processed:
             # API check RFID
-            response_check_rfid = requests.post(url_check_rfid, data={'rfid': rfid_uid})
+            response_check_rfid = requests.post(
+                url_check_rfid, data={'rfid': rfid_uid})
             if response_check_rfid.status_code == 200:
                 print("RFID is registered in the database.")
                 print("RFID Number:", rfid_uid)
@@ -71,16 +257,17 @@ def handle_rfid_scan():
                 points = response_check_rfid.json().get('points', 0)
 
                 print("Points:", points)
-                
+
                 # Update the global points variable
-                global_points = points  
+                global_points = points
 
                 # Update points on the UI
                 update_points()
 
                 # Rest of the code for processing when RFID is registered...
             else:
-                print("RFID is not registered in the database. Attempt", current_attempt)
+                print("RFID is not registered in the database. Attempt",
+                      current_attempt)
                 # Additional actions when RFID is not registered...
 
             # Set the flag to False to prevent further scans until the next update
@@ -98,9 +285,11 @@ def handle_rfid_scan():
         GPIO.cleanup()
 
 # Handle RFID scan in get points
+
+
 def handle_rfid_scan_get_points():
     global global_points, scan_processed
-    
+
     try:
         # RFID setup
         reader = SimpleMFRC522()
@@ -112,7 +301,8 @@ def handle_rfid_scan_get_points():
         # Process scan only if the flag is True
         if scan_processed:
             # API check RFID
-            response_check_rfid = requests.post(url_check_rfid, data={'rfid': rfid_uid})
+            response_check_rfid = requests.post(
+                url_check_rfid, data={'rfid': rfid_uid})
             if response_check_rfid.status_code == 200:
                 print("RFID is registered in the database.")
                 print("RFID Number:", rfid_uid)
@@ -120,16 +310,17 @@ def handle_rfid_scan_get_points():
                 points = response_check_rfid.json().get('points', 0)
 
                 print("Points:", points)
-                
+
                 # Update the global points variable
-                global_points = points  
+                global_points = points
 
                 # Update points on the UI
                 update_points()
 
                 # Rest of the code for processing when RFID is registered...
             else:
-                print("RFID is not registered in the database. Attempt", current_attempt)
+                print("RFID is not registered in the database. Attempt",
+                      current_attempt)
                 # Additional actions when RFID is not registered...
 
             # Set the flag to False to prevent further scans until the next update
@@ -148,23 +339,33 @@ def handle_rfid_scan_get_points():
         app.after(1000, process_plastic_bottles)
 
 # Function to exit the program
+
+
 def exit_program():
     print("Exiting program...")
     app.destroy()
 
 # Function to get the script's directory
+
+
 def get_script_directory():
     return os.path.dirname(os.path.abspath(__file__))
 
 # Function to construct a relative path to an image file
+
+
 def get_image_path(image_filename):
     return os.path.join(get_script_directory(), image_filename)
 
 # Testing
+
+
 def trigger_script():
     os.system('python hello.py')
 
 # Trigger claim points
+
+
 def claim_points():
     os.system('python hello.py')
 
@@ -181,6 +382,7 @@ def destroy_elements_scan_rfid():
 
     menu()
 
+
 def destroy_elements_tutorial():
     revendo_logo.place_forget()
     get_points_button.place_forget()
@@ -189,6 +391,7 @@ def destroy_elements_tutorial():
     image_tutorial.place_forget()
 
     menu()
+
 
 def destroy_elements_process():
     revendo_logo.place_forget()
@@ -207,10 +410,11 @@ def destroy_elements_process():
     smallbottle_text.place_forget()
     cancel_button.place_forget()
     claim_button.place_forget()
-    
+
     points_var.set(str(""))
 
     menu()
+
 
 def destroy_elements_check_points():
     balance_header.place_forget()
@@ -219,18 +423,21 @@ def destroy_elements_check_points():
 
     menu()
 
+
 def destroy_elements_check_balance():
     tutorial_header.place_forget()
     image_label.place_forget()
     balance_label.place_forget()
     points_entry.place_forget()
     okay_button.place_forget()
-    
+
     points_var.set(str(""))
-    
+
     menu()
 
 # Get points scan rfid
+
+
 def get_points_scan_rfid_page():
     global revendo_logo, get_points_button, check_balance_button, image_button, image_label, new_button, tutorial_header, tutorial_steps, button
 
@@ -256,10 +463,12 @@ def get_points_scan_rfid_page():
     get_points_button.place_forget()
     check_balance_button.place_forget()
     image_button.place_forget()
-    
+
     app.after(1000, handle_rfid_scan_get_points)
 
 # Process plastic bottle page
+
+
 def process_plastic_bottles():
     global your_balance_header, image_label_process, largebottle_text, balance_text, smallbottle_text, totalplasticbottle_text, mediumbottle_text, totalpoints_text, smallbottle_text, cancel_button, claim_button, points_entry
 
@@ -276,9 +485,10 @@ def process_plastic_bottles():
     image_label_process.place(relx=0.3, rely=0.55, anchor='center')
 
     # Entry (textbox) to display points
-    points_entry = Entry(app, textvariable=points_var, font=("Helvetica", 16), state='readonly', readonlybackground='white', justify='center')
+    points_entry = Entry(app, textvariable=points_var, font=(
+        "Helvetica", 16), state='readonly', readonlybackground='white', justify='center')
     points_entry.place(relx=0.5, rely=0.1, anchor='center',
-                     width=300, height=40)
+                       width=300, height=40)
 
     largebottle_text = tk.Entry(app, state='disabled', font=(
         "Arial", 16), bg='#f0f0f0', justify='center')
@@ -359,6 +569,7 @@ def process_plastic_bottles():
     image_label.place_forget()
     new_button.place_forget()
 
+
 def check_points_scan_rfid_page():
     global revendo_logo, get_points_button, check_balance_button, image_button, image_label, new_button, tutorial_header, tutorial_steps, button, balance_label, points_entry, okay_button
 
@@ -373,16 +584,18 @@ def check_points_scan_rfid_page():
     image_label = tk.Label(app, image=photo)
     image_label.image = photo
     image_label.place(relx=0.5, rely=0.4, anchor='center')
-    
+
     # Label to display "Balance"
-    balance_label = Label(app, text="Your Balance", font=("Helvetica", 14), bg='#8599e0', fg='white')
+    balance_label = Label(app, text="Your Balance", font=(
+        "Helvetica", 14), bg='#8599e0', fg='white')
     balance_label.place(relx=0.5, rely=0.6, anchor='center',
-                     width=250, height=40)
+                        width=250, height=40)
 
     # Entry (textbox) to display points
-    points_entry = Entry(app, textvariable=points_var, font=("Helvetica", 16), state='readonly', readonlybackground='white', justify='center')
+    points_entry = Entry(app, textvariable=points_var, font=(
+        "Helvetica", 16), state='readonly', readonlybackground='white', justify='center')
     points_entry.place(relx=0.5, rely=0.7, anchor='center',
-                     width=250, height=40)
+                       width=250, height=40)
 
     # Okay button
     okay_button = tk.Button(app, text="Okay", font=(
@@ -397,8 +610,9 @@ def check_points_scan_rfid_page():
     image_button.place_forget()
 
     # Trigger RFID scan explicitly
-    #handle_rfid_scan()
+    # handle_rfid_scan()
     app.after(1000, handle_rfid_scan)
+
 
 def check_points_page():
     global balance_header, balance_text, button, global_points, balance_label, points_entry, okay_button
@@ -406,11 +620,12 @@ def check_points_page():
     balance_header = tk.Label(app, text="Your Balance", font=(
         "Arial", 24), bg='#8599e0', fg='white')
     balance_header.place(relx=0.5, rely=0.4, anchor='center')
-    
+
     # Entry (textbox) to display points
-    points_entry = Entry(app, textvariable=points_var, font=("Helvetica", 16), state='readonly', readonlybackground='white', bg='#8599e0', justify='center')
+    points_entry = Entry(app, textvariable=points_var, font=(
+        "Helvetica", 16), state='readonly', readonlybackground='white', bg='#8599e0', justify='center')
     points_entry.place(relx=0.5, rely=0.5, anchor='center',
-                     width=400, height=40)
+                       width=400, height=40)
 
     balance_text = tk.Entry(app, state='disabled', font=(
         "Arial", 16), bg='#8599e0', justify='center')
@@ -429,6 +644,7 @@ def check_points_page():
     new_button.place_forget()
     image_label.place_forget()
 
+
 def show_image_modal():
     # Load the image for the pop-up
     modal_image_path = get_image_path("rfid-reader1.jpg")
@@ -446,14 +662,17 @@ def show_image_modal():
     modal_image_label.pack()
 
     # Add an "OK" button to close the modal
-    ok_button = tk.Button(modal_window, text="OK", command=modal_window.destroy)
+    ok_button = tk.Button(modal_window, text="OK",
+                          command=modal_window.destroy)
     ok_button.pack()
+
 
 def update_balance_text(value):
     balance_text.config(state='normal')
     balance_text.delete(0, 'end')  # Clear the current value
     balance_text.insert(0, str(value))  # Insert the new value
     balance_text.config(state='disabled')
+
 
 def tutorial_page():
     global revendo_logo, label, get_points_button, check_balance_button, image_button, image_label, new_button, tutorial_header, tutorial_steps, button, image_tutorial, image_tutorial_path
@@ -512,6 +731,7 @@ def menu():
     image_button.image = photo_button
     image_button.place(relx=0.5, rely=0.75, anchor='center')
 
+
 # Maximum number of attempts allowed
 max_attempts = 3
 current_attempt = 1
@@ -532,8 +752,8 @@ background_label = tk.Label(app, image=background_image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Set the application to run in full screen - uncomment this if you are in raspberry pi
-#app.attributes('-fullscreen', True)
-#app.wm_attributes('-fullscreen', True)
+# app.attributes('-fullscreen', True)
+# app.wm_attributes('-fullscreen', True)
 
 # Run the application
 menu()
