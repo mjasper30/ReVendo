6a0cf9c9e36b19424a8e2ece1150c2e607567a40
mjasper30 <jasper.macaraeg42@gmail.com>
2024-02-26 22:51:37 +0800

    adjust ui

--- frontend/client/src/loader.tsx
+++ frontend/client/src/loader.tsx
@@ -6,17 +6,17 @@ function Loader() {
     <>
       <div className="loader">
         <div className="bg-background fixed inset-0 bg-cover bg-center fade-in">
-          <div className="flex justify-center items-center flex-col pt-11">
+          <div className="flex justify-center items-center flex-col pt-11 sm:pt-52">
             <img
-              className="firstLoop bounce2 mx-auto w-96 h-96"
+              className="firstLoop bounce2 mx-auto w-96 h-96 sm:w-[120px] sm:h-[120px]"
               src={loader}
               alt="ReVendo Logo"
             />
             <div className="content mt-14 text-center">
-              <h2 className="text-2xl mt-5 md:text-3xl lg:text-4xl xl:text-5xl">
+              <h2 className="text-2xl mt-5 sm:text-sm md:text-3xl lg:text-4xl xl:text-5xl">
                 ReVendo
               </h2>
-              <h2 className="text-2xl mt-5 md:text-3xl lg:text-4xl xl:text-5xl">
+              <h2 className="text-2xl mt-5 sm:text-sm md:text-3xl lg:text-4xl xl:text-5xl">
                 ReVendo
               </h2>
             </div>
