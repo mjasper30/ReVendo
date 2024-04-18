adf060b9996dd2d8a9cb30df786710fcf99cb3d5
mjasper30 <jasper.macaraeg42@gmail.com>
2024-04-18 22:51:16 +0800

    clean up files

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -3,10 +3,8 @@ import Login from "./Login";
 import Dashboard from "./Dashboard";
 import Settings from "./Settings";
 import RFID from "./RFID";
-import Units from "./Units";
 import Accounts from "./Accounts";
 import History from "./History";
-import BinAlert from "./BinAlert";
 
 function App() {
   return (
@@ -14,12 +12,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />}></Route>
-          <Route path="/bin-alert" element={<BinAlert />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
           <Route path="/settings" element={<Settings />}></Route>
           <Route path="/rfid" element={<RFID />}></Route>
           <Route path="/manage-accounts" element={<Accounts />}></Route>
-          <Route path="/units" element={<Units />}></Route>
           <Route path="/history" element={<History />}></Route>
         </Routes>
       </BrowserRouter>
